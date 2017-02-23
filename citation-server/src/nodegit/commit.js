8cee143d97f7bb2e280aae626813e1e2b9a50551
ChloeMoulin <chloe.moulin42@free.fr>
2017-02-23 11:31:33 +0100

    commit date fixes

--- citation-server/src/nodegit/commit.js
+++ citation-server/src/nodegit/commit.js
@@ -1,3 +1,4 @@
+import os from 'os';
 import {Reference, Signature} from 'nodegit';
 import winston from 'winston';
 
@@ -7,8 +8,9 @@ export default async function commit(oid) {
 	try {
 		const head = await Reference.nameToId(this.repository, 'HEAD');
 		const parent = await this.repository.getCommit(head);
-		const author = Signature.create('Citation', 'citation@aya-experience.com', new Date().getTime(), 0);
-		const commitId = await this.repository.createCommit('HEAD', author, author, 'Update from Citation server', oid, [parent]);
+		const hostname = os.hostname();
+		const author = Signature.create('Citation', 'citation@aya-experience.com', new Date().getTime() / 1000 | 0, 0);
+		const commitId = await this.repository.createCommit('HEAD', author, author, `Update from Citation server - From host : ${hostname}`, oid, [parent]);
 		return commitId;
 	} catch (error) {
 		logger.error(`NodeGit commit error ${error}`);
