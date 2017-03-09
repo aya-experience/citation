fa619a10bc653b24330669c98d03c9dd79b5345d
Swiip <matthieu.lux@gmail.com>
2017-03-09 15:26:14 +0100

    fix server url in production

--- citation-demo/src/index.js
+++ citation-demo/src/index.js
@@ -1,3 +1,5 @@
+/* global process */
+
 import 'babel-polyfill';
 
 import React from 'react';
@@ -8,9 +10,17 @@ import CitationRouter from 'citation-react-router';
 import components from './components';
 import './index.css';
 
+let serverUrl;
+
+if (process && process.env && process.env.NODE_ENV === 'development') {
+	serverUrl = 'http://localhost:4000/graphql';
+} else {
+	serverUrl = '/graphql';
+}
+
 ReactDOM.render(
 	<BrowserRouter>
-		<CitationRouter serverUrl="http://localhost:4000/graphql" components={components}/>
+		<CitationRouter serverUrl={serverUrl} components={components}/>
 	</BrowserRouter>,
 	document.getElementById('root')
 );
