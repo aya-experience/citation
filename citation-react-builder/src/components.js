aaa47fa24e6a75bfcde0fec9333bab8b78315d67
Swiip <matthieu.lux@gmail.com>
2017-06-03 15:40:46 +0200

    reset components for travis to build

--- citation-react-builder/src/components.js
+++ citation-react-builder/src/components.js
@@ -1,9 +1,1 @@
-import components0 from '../../citation-react-demo-components';
-import components1 from '../../citation-react-demo/work/components/1';
-import Particles from '../../citation-react-demo/node_modules/react-particles-js';
-
-export default {
-	...components0,
-	...components1,
-	Particles
-};
+export default {};
