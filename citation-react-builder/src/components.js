35cde40d5209141479e28c4f57250c958b7532c1
Swiip <matthieu.lux@gmail.com>
2017-09-14 18:17:02 +0200

    fix default components

--- citation-react-builder/src/components.js
+++ citation-react-builder/src/components.js
@@ -1,9 +1,1 @@
-import components0 from 'citation-react-demo-components';
-import components1 from 'citation-components-1';
-import Particles from 'react-particles-js';
-
-export default {
-	...components0,
-	...components1,
-	Particles
-};
+export default {};
